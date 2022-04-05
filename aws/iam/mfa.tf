cc1de6b735f4f9bb15d53fa6febe68aa31b19e34
Abhi Yerra <abhi@berkeley.edu>
2022-04-05 14:53:07 -0700

    Readd aws_account_id

--- aws/iam/mfa.tf
+++ aws/iam/mfa.tf
@@ -1,3 +1,9 @@
+data "aws_caller_identity" "current" {}
+
+locals {
+  aws_account_id = data.aws_caller_identity.current.account_id
+}
+
 resource "aws_iam_policy" "mfa" {
   name        = "${var.prefix}MFA"
   path        = "/"

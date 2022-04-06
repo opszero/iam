509f44a51cccd31514b2349def16c49457d83c90
Abhi Yerra <abhi@berkeley.edu>
2022-04-05 18:45:31 -0700

    Fix policy lookup

--- modules/aws/github.tf
+++ modules/aws/github.tf
@@ -13,5 +13,5 @@ module "oidc-github" {
   attach_read_only_policy = false
 
   iam_role_name        = "${each.key}-github"
-  iam_role_policy_arns = lookup(each.value.policy_arns, [])
+  iam_role_policy_arns = lookup(each.value, "policy_arns", [])
 }

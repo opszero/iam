6f5db65606efbb26ee0f1051aff277561fb0e858
swezinlinn <swezinlinn1@gmail.com>
2022-04-20 10:42:58 +0630

    update

--- variables.tf
+++ variables.tf
@@ -1,32 +1,42 @@
 variable "groups" {
-  default = {}
-  # name = {
-  #   policy_arns = []
-  #   enable_mfa = false
-  # }
+  default = {
+    "developers" = {
+      policy_arns = []
+      enable_mfa  = false
+    }
+  }
 }
 
 variable "users" {
-  default = {}
-  # "opszero" = {
-  #  groups = ["developers"]
-  # }
+  default = {
+    "opszero" = {
+      groups = ["developers"]
+    }
+  }
 }
 
 variable "github" {
-  default = {}
-  # "deployer" = {
-  #   org = "opszero"
-  #   repos = ["mrmgr"]
-  #   policy_arns = []
-  # }
+  default = {
+    "deployer" = {
+      org         = "thaunghtike-share"
+      repos       = ["mytfdemo", "terraform-aws-mrmgr"]
+      policy_arns = []
+    }
+  }
 }
 
 variable "gitlab" {
-  default = {}
-  # "opszero/mrmgr" = {
-  #   name = "mrmgr"
-  #   branch = "master"
-  #   path = "mrmgr"
-  # }
+  default = {
+    "deployer" = {
+      match_field = "sub"
+      match_value = [
+        "project_path:thaunghtikeoo/demo:ref_type:branch:ref:main",
+        "project_path:thaunghtikeoo/oidc-demo:ref_type:branch:ref:main"
+      ]
+      policy_arns = [
+
+      ]
+    }
+  }
 }
+

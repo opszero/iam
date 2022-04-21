c735380d76d72a98723245aeb261a0adf9a51e9b
thaunghtike-share <87596555+thaunghtike-share@users.noreply.github.com>
2022-04-21 09:40:23 +0630

    Update gitlab.tf

--- gitlab.tf
+++ gitlab.tf
@@ -6,10 +6,10 @@ module "aws_oidc_gitlab" {
 
   attach_admin_policy  = false
   create_oidc_provider = true
-  iam_role_name        = each.iam_role_name
+  iam_role_name        = each.value.iam_role_name
   iam_policy_arns      = each.value.policy_arns
-  gitlab_url           = each.gitlab_oidc_url
-  audience             = each.audience
+  gitlab_url           = each.value.gitlab_oidc_url
+  audience             = each.value.audience
   match_field          = each.value.match_field
   match_value          = each.value.match_value
 }

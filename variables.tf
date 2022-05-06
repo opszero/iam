7e50b044937b4b86d1a9a00e28c81a4ff4907ec2
thaunghtike-share <87596555+thaunghtike-share@users.noreply.github.com>
2022-05-06 11:43:21 +0630

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,43 +1,40 @@
 variable "groups" {
-  default = {
-    "developers" = {
-      policy_arns = []    # set group policies arn from group_policies.tf here
-      enable_mfa  = false
-    }
-  }
+  default = {}
+  # name = {
+  #   policy_arns = []
+  #   enable_mfa = false
+  # }
 }
 
 variable "users" {
-  default = {
-    "opszero" = {
-      groups = ["developers"]
-    }
-  }
+  default = {}
+  # "opszero" = {
+  #  groups = ["developers"]
+  # }
 }
 
 variable "github" {
-  default = {
-    "deployer" = {
-      org         = "opszero"
-      repos       = ["mrmgr"]
-      policy_arns = []
-    }
-  }
+  default = {}
+  # "deployer" = {
+  #   org = "opszero"
+  #   repos = ["mrmgr"]
+  #   policy_arns = []
+  # }
 }
 
 variable "gitlab" {
-  default = {
-    "deployer" = {
-      iam_role_name = "gitlab_oidc_role"
-      gitlab_url    = "https://gitlab.com"
-      audience      = "https://gitlab.com"
-      match_field   = "sub"
-      match_value   = [
-        "project_path:opszero/mrmgr:ref_type:branch:ref:main"
-      ]
-      policy_arns   = []
-    }
-  }
+  default = {}
+  #  "deployer" = {
+  #    iam_role_name = "gitlab_oidc_role"
+  #    gitlab_url    = "https://gitlab.com"
+  #    audience      = "https://gitlab.com"
+  #    match_field   = "sub"
+  #    match_value   = [
+  #      "project_path:opszero/mrmgr:ref_type:branch:ref:main"
+  #    ]
+  #    policy_arns   = []
+  #  }
+  # }
 }
 
 

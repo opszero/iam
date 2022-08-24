4211bcaa13164d70e1d97f75b8350cf86aae1fa1
Thaung Htike Oo <thaunghtikeoo@Thaungs-MacBook-Air.local>
2022-08-25 00:43:54 +0630

    update IAM users with policy arns

--- iam_user_policies/iam_user_policy_attach.tf
+++ iam_user_policies/iam_user_policy_attach.tf
@@ -1,5 +1,13 @@
+resource "aws_iam_user" "user" {
+  name = var.users
+}
+
 resource "aws_iam_user_policy_attachment" "this" {
   count      = length(var.user_policy_arns)
   user       = var.users
   policy_arn = var.user_policy_arns[count.index]
+
+  depends_on = [
+    aws_iam_user.user
+  ]
 }
--- users.tf
+++ users.tf
@@ -1,16 +1,7 @@
-resource "aws_iam_user" "user" {
-  for_each = var.users
-  name     = each.key
-}
-
 module "iam_user_policies" {
   for_each = var.users
   source   = "./iam_user_policies"
 
   users            = each.key
   user_policy_arns = each.value.user_policy_arns
-
-  depends_on = [
-    aws_iam_user.user
-  ]
 }

fe95c829e0b6a6ee2078013cccd4c641d2542741
Thaung Htike Oo <thaunghtikeoo@Thaungs-MacBook-Air.local>
2022-08-25 00:23:06 +0630

    update IAM users with policy arns

---
+++ iam_user_policies/iam_user_policy_attach.tf
@@ -0,0 +1,5 @@
+resource "aws_iam_user_policy_attachment" "this" {
+  count      = length(var.user_policy_arns)
+  user       = var.users
+  policy_arn = var.user_policy_arns[count.index]
+}
---
+++ iam_user_policies/variables.tf
@@ -0,0 +1,12 @@
+variable "users" {
+  type        = string
+  default     = ""
+  description = "The name of IAM user"
+}
+
+variable "user_policy_arns" {
+  type        = list(any)
+  default     = []
+  description = "List of IAM policy ARNs"
+
+}

c440f81209dfac6437d54ca121d4c01b68119c8a
Abhi Yerra <abhi@berkeley.edu>
2022-08-26 16:27:47 -0700

    Add lookup

--- users.tf
+++ users.tf
@@ -3,5 +3,5 @@ module "iam_users_with_policies" {
   source   = "./iam_users_with_policies"
 
   user             = each.key
-  user_policy_arns = each.value.user_policy_arns
+  user_policy_arns = lookup(each.value, "user_policy_arns", [])
 }

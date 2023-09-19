19b122d131a9f0874528880c3d9e8987690c6091
Abhi Yerra <abhi@berkeley.edu>
2023-09-19 13:26:57 -0700

    Added Bitbucket

--- bitbucket.tf
+++ bitbucket.tf
@@ -10,12 +10,12 @@ module "bitbucket" {
   roles = {
     role1 = {
       name             = "bitbucket-${each.key}"
-      allowed_subjects = ["{xxxxxxxx-xxxx-xxxx-xxxxxxxxxxxxxxxxx}*"]
+      allowed_subjects = lookup(each.value, "subjects", [])
       # This is matched against the sub claim, which provided in the following format:
       # {REPOSITORY_UUID}[:{ENVIRONMENT_UUID}]:{STEP_UUID}
       # More info here: https://support.atlassian.com/bitbucket-cloud/docs/deploy-on-aws-using-bitbucket-pipelines-openid-connect/#Using-claims-in-ID-tokens-to-limit-access-to-the-IAM-role-in-AWS
 
-      inline_policies_json = [data.aws_iam_policy_document.pipeline_role.json]
+      inline_policies_json = lookup(each.value, "policy_json", [])
       # You can either add policies inline here or add them via aws_iam_role_policy_attachment resource
     }
   }
